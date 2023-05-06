4386a51f94b4df8b6630f05743a01abbad41d9d0
Beusts <96443991+Beusts@users.noreply.github.com>
2023-05-06 14:23:53 +0200

    Update Sql.java

--- Sql.java
+++ Sql.java
@@ -1,17 +1,17 @@
 public class Cabinet {
 	public static String adresse = "sql7.freemysqlhosting.net";
-	public static String bd = "sql7614286";
-	public static String login = "sql7614286";
-	public static String password = "K55YjyNVWK";
+	public static String bd = "sql7615754";
+	public static String login = "sql7615754";
+	public static String password = "Gp8NFVRvEz";
 
 	public static void main(String[] args) {
 		int connexion = BD.ouvrirConnexion(adresse, bd, login,password);
-		// cr ´eation de la requ^ete
+		// cr Â´eation de la requ^ete
 		String sql = "SELECT NumSecu, PATIENT.Nom, COUNT(NumCons) FROM CONSULTATION, PATIENT WHERE"
 		+" CONSULTATION.Patient = NumSecu GROUP BY Nom";
 		// envoi de la requ^ete
 		int res = BD.executerSelect(connexion, sql);
-		// parcours du r ´esultat (ligne par ligne)
+		// parcours du r Â´esultat (ligne par ligne)
 		while (BD.suivant(res)) {
 			int numCons = BD.attributInt(res,"COUNT(NumCons)");
 			String NumSecu = BD.attributString(res,"NumSecu");
